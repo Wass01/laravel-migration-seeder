7084a176833086d5a6a179a6f6a9f9d2707babe9
wass01 <maatallawassim01@gmail.com>
2021-05-19 15:32:30 +0200

    migration created

---
+++ database/migrations/2021_05_19_131104_create_travels_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string('destination', 100);
+            $table->smallInteger('num_people')->unsigned();
+            $table->string('type', 50);
+            $table->date('date');
+            $table->text('description')->nullable();
+            $table->float('price', 10, 2);
+            $table->timestamps();
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
